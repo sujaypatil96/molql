4ade5ad3fa90423a968702e6758147af4278f75e
Sujay Patil <sujaysanjeev.patil@gmail.com>
2018-03-20 10:18:08 +0000

    regEx parsers for PyMol commands impl

---
+++ src/transpilers/pymol/commands.ts
@@ -0,0 +1,13 @@
+// map the PyMol command to your MolQL symbols here
+
+import { CommandDict } from '../types'
+import B from '../../molql/builder'
+import * as h from '../helper'
+
+const commands: CommandDict = {
+
+    select: {
+        // code
+        // perhaps using atomGroups() or any other symbols
+    }
+}
