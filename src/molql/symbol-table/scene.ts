8672c33a18329bf7c85b9a00c86b39e31b56b989
Sujay Patil <sujaysanjeev.patil@gmail.com>
2018-03-21 13:14:25 +0000

    updated scene with template symbol

--- src/molql/symbol-table/scene.ts
+++ src/molql/symbol-table/scene.ts
@@ -1,1 +1,19 @@
-// declare new symbols needed for the commands here  
+// declare new symbols needed for the commands here
+
+import Type from '../type'
+import * as Core from './core'
+import { Arguments, Argument } from '../symbol'
+import { symbol } from '../helpers'
+import { Types } from './structure'
+
+const command = {
+    '@header' : 'Commands',
+    // add 'select' command as a Symbol
+    // for this we will just be using atomGroups function
+    // select: symbol();
+}
+
+export default {
+    '@header': 'Scene Queries',
+    command
+}
