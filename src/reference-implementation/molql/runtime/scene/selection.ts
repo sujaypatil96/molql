827694be970e7c069713a5043a663b686276d3e6
Sujay Patil <sujaysanjeev.patil@gmail.com>
2018-03-21 13:05:37 +0000

    symbols for scene selection

---
+++ src/reference-implementation/molql/runtime/scene/selection.ts
@@ -0,0 +1,16 @@
+// provide implementation for all the 'scene' symbols
+
+import Environment from '../environment'
+import Expression from '../expression'
+import { FastMap, FastSet, sortAsc } from '../../../utils/collections'
+import AtomSet from '../../data/atom-set'
+import AtomSelection from '../../data/atom-selection'
+import ElementAddress from '../../data/element-address'
+import Context from '../context'
+import { Model } from '../../../structure/data'
+import Mask from '../../../utils/mask'
+
+// implementation for select scene function
+function selectScene() {
+
+}
