9ab78a03270b8a344969ecbd9dbe71fd492c0716
Sujay Patil <sujaysanjeev.patil@gmail.com>
2018-03-26 11:51:02 +0100

    placeholder transformation for "select"

--- src/transpilers/pymol/commands.ts
+++ src/transpilers/pymol/commands.ts
@@ -1,13 +1,18 @@
-// map the PyMol command to your MolQL symbols here
+// // map the PyMol command to your MolQL symbols here
 
 import { CommandDict } from '../types'
 import B from '../../molql/builder'
-import * as h from '../helper'
+// import * as h from '../helper'
 
 const commands: CommandDict = {
-
     select: {
         // code
         // perhaps using atomGroups() or any other symbols
+        '@desc' : 'selection of atoms depending on the selection-expression',
+        '@examples' : ['select near142, resi 142 around 5'],
+        name: 'select',
+        map: () => B.struct.generator.atomGroups()
     }
 }
+
+export default commands
