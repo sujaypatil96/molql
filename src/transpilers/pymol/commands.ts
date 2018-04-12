c027a130e2398d818272000985a9a454c2f510c4
Sujay Patil <sujaysanjeev.patil@gmail.com>
2018-04-12 15:03:58 +0100

    type specified for "select" symbol

--- src/transpilers/pymol/commands.ts
+++ src/transpilers/pymol/commands.ts
@@ -4,20 +4,49 @@ import { CommandDict } from '../types'
 import B from '../../molql/builder'
 // import * as h from '../helper'
 
+// const reFloat = /[-+]?[0-9]*\.?[0-9]+/
+// const rePosInt = /[0-9]+/
+
+function atomListMap(x: string) { return x.split('+').map(B.rand) }
+
+// function atomNameListMap(x: string) { return x.split('+').map(B.atomName) }
+
+function listMap(x: string) { return x.split('+').map(x => x.replace(/^["']|["']$/g, '')) }
+
+// function atomListMap(x: string[]) {
+//     x.forEach(x => {
+//         return x.split(' ').map(B.atomName)
+//     })
+// }
+
+// function rangeMap(x: string) {
+//   const [min, max] = x.split('-').map(x => parseInt(x))
+//   return {min, max}
+// }
+// function listOrRangeMap(x: string) {
+//   return x.includes('-') ? rangeMap(x) : listMap(x).map(x => parseInt(x))
+// }
+
 const commands: CommandDict = {
-  // perhaps using atomGroups() or any other symbols
+    // perhaps using atomGroups() or any other symbols
     select: {
-        '@desc' : 'selection of atoms depending on the selection-expression',
-        '@examples' : ['select near142, resi 142 around 5'],
-        name : 'select',
-        map: () => B.struct.generator.atomGroups()  // random implementation for now
+      '@desc': 'select atom-groups based on selection expression with appropriate property selector (name).',
+      abbr: ['select'],
+      regex: /[a-zA-Z '"+]+/,
+      map: atomListMap,
+      level: 'atom-test',
+      property: B.ammp('label_atom_id')
     },
-    show: {
-      '@desc' : 'specify the representation for the current selection',
-      '@examples' : ['show ribbon'],
-      name : 'show',
-      map: () => B.struct.generator.nthRes()  // random implementation for now
+
+    "select resn": {
+      '@desc': 'select atom-groups based on selection expression with appropriate property selector (resn).',
+      abbr: ['select resn'],
+      regex: /[a-zA-Z'"+]+/,
+      map: listMap,
+      level: 'residue-test',
+      property: B.ammp('label_comp_id')
     }
+
 }
 
 export default commands
