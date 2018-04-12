7669b3bf703b45a2e674c7c088e99143e3ca5100
Sujay Patil <sujaysanjeev.patil@gmail.com>
2018-04-12 15:02:06 +0100

    added select symbol

--- src/molql/symbol-table/scene.ts
+++ src/molql/symbol-table/scene.ts
@@ -1,16 +1,20 @@
 // declare new symbols needed for the commands here
 
 import Type from '../type'
-import * as Core from './core'
 import { Arguments, Argument } from '../symbol'
 import { symbol } from '../helpers'
 import { Types } from './structure'
 
 const command = {
-    '@header' : 'Commands',
-    // add 'select' command as a Symbol
-    // for this we will just be using atomGroups function
-    // select: symbol();
+    '@header': 'commands',
+    select: symbol(Arguments.Dictionary({
+        'property-selector' : Argument(Type.Any, { isOptional: true, defaultValue: "name", description: 'Property used for selection of atom-groups.' }),
+        'entity-test': Argument(Type.Bool, { isOptional: true, defaultValue: true, description: 'Test for the 1st atom of every entity' }),
+        'chain-test': Argument(Type.Bool, { isOptional: true, defaultValue: true, description: 'Test for the 1st atom of every chain'  }),
+        'residue-test': Argument(Type.Bool, { isOptional: true, defaultValue: true, description: 'Test for the 1st atom every residue'  }),
+        'atom-test': Argument(Type.Bool, { isOptional: true, defaultValue: true }),
+        'group-by': Argument(Type.Any, { isOptional: true, defaultValue: `atom-key`, description: 'Group atoms to sets based on this property. Default: each atom has its own set' }),
+    }), Types.AtomSelectionQuery, 'Symbol for PyMol select command. Uses AtomGroups function for now.')
 }
 
 export default {
